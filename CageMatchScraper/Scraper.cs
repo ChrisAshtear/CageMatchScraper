d7542239f72f6a5e183fd1b6bb95f020b55688aa
ChrisAshtear <chrisashtear@gmail.com>
2022-06-02 01:06:15 -0400

    event parsing mostly works

--- CageMatchScraper/Scraper.cs
+++ CageMatchScraper/Scraper.cs
@@ -15,7 +15,62 @@ namespace CageMatchScraper
         EVENT = 1,
         WRESTLER = 2,
         PROMOTION = 8,
-        TITLE = 5
+        TITLE = 5,
+        TAGTEAM = 28
+    }
+    
+    public enum PageType
+    {
+        OVERVIEW=-1,
+        NEWS=2,
+        EVENTS=4,
+        EVENTSTATS=19,
+        RESULTS=8,
+        TITLES=9,
+        ROSTER=15,
+        ALLTIMEROSTER=16,
+        MATCHGUIDE=7,
+        WINLOSS=17,
+        PROMOS=6,
+        TOURNAMENTS=11,
+        RIVALRIES=14,
+        RATINGS=98
+    }
+
+    public struct TagInfo
+    {
+        public string htmlElement;
+        public string className;
+    }
+
+    public class TagTeam
+    {
+        public string name;
+        public List<Wrestler> wrestlers = new List<Wrestler>();
+    }
+
+    public class Wrestler
+    {
+        public string name;
+        public int wrestlerID;
+    }
+
+    public class WrestlingMatch
+    {
+        public List<List<Wrestler>> sides = new List<List<Wrestler>>();
+        public string length;
+        public int victor = 0;
+        public string data;
+    }
+
+    public class WrestlingEvent
+    {
+        public string name;
+        public string location;
+        public string date;
+        public int eventID;
+
+        public List<WrestlingMatch> matches = new List<WrestlingMatch>();
     }
 
     public class Scraper
@@ -34,9 +89,10 @@ namespace CageMatchScraper
             return await response;
         }
 
-        public static string GetEntry(RequestType type, int promotionID)
+        public static string GetEntry(RequestType type, int promotionID, PageType pageNum = PageType.OVERVIEW)
         {
             string url = baseURL + "id="+(int)type+"&nr=" + promotionID;
+            if(pageNum >= 0) { url += "&page=" + (int)pageNum; }
             return CallUrl(url).Result;
         }
 
@@ -64,5 +120,111 @@ namespace CageMatchScraper
 
         }
 
+        public string Between(string STR, string FirstString, string LastString="NONE")
+        {
+            string FinalString = "";
+            try
+            {
+                int Pos1 = STR.IndexOf(FirstString) + FirstString.Length;
+                int Pos2 = STR.IndexOf(LastString, Pos1);
+                if (LastString == "NONE") { Pos2 = STR.Length; }
+                if (Pos2 - Pos1 < 0) { return ""; }
+                FinalString = STR.Substring(Pos1, Pos2 - Pos1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + STR);
+                FinalString = "";
+            }
+            
+            return FinalString;
+        }
+
+        public Dictionary<string, string> ParseList(string html, TagInfo eventTag, TagInfo listHeader, TagInfo listEntry)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var eventEntry = htmlDoc.DocumentNode.Descendants(eventTag.htmlElement)
+                    .Where(node => node.GetAttributeValue("class", "").Equals(eventTag.className)).ToList();
+
+            Dictionary<string,WrestlingEvent> events = new Dictionary<string,WrestlingEvent>();
+
+            foreach(var item in eventEntry)
+            {
+                var name = item.Descendants(listHeader.htmlElement)
+                    .Where(node => node.GetAttributeValue("class", "").Contains(listHeader.className)).ToList();
+
+                WrestlingEvent evt = new WrestlingEvent();
+                evt.name = Between(name[0].InnerHtml,">","<");
+                evt.date = Between(name[0].InnerHtml, "(", ")");
+                evt.location = Between(name[0].InnerHtml, "@");
+                evt.eventID = int.Parse(Between(name[0].InnerHtml, "nr=", "\""));
+
+                var values = item.Descendants(listEntry.htmlElement)
+                        .Where(node => node.GetAttributeValue("class", "").Contains(listEntry.className)).ToList();
+
+                foreach(var value in values)
+                {
+                    WrestlingMatch match = new WrestlingMatch();
+                    match.data = value.InnerHtml;
+                    String[] sides = value.InnerHtml.Split("defeat");
+                    
+                    foreach(string side in sides)
+                    {
+                        List<Wrestler> participants = new List<Wrestler>();
+
+                        string teamEntry = Between(side, "(", ")");
+                        string otherMembers = side;
+                        if (side.Contains("(") && teamEntry.Length > 5)// tag team, or match length.
+                        {
+                            //string teamEntry = Between(side, "(", ")");
+                            TagTeam team = new TagTeam();
+                            team.name = Between(side, ">", "<");
+                            List<Wrestler> teamMembers = ParseParticipants(teamEntry);
+                            participants.AddRange(teamMembers);
+                            otherMembers = Between(side, ")");
+                        }
+                        if(otherMembers.Length >3)
+                        {
+                            List<Wrestler> singleMembers = ParseParticipants(otherMembers);
+                            participants.AddRange(singleMembers);
+                        }
+                        match.sides.Add(participants);
+                    }
+                    //w/ Manager - managers still counted as participants.
+                    evt.matches.Add(match);
+                }
+                events.Add(evt.name,evt);
+            }
+
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            /*
+            for (int i = 0; i < names.Count; i++)
+            {
+                var name = names[i];
+                var value = values[i];
+                data.Add(name.InnerHtml.Trim(':'), value.InnerHtml);
+            }*/
+
+            return data;
+
+        }
+        List<Wrestler> ParseParticipants(string html)
+        {
+            string[] wrestlers = html.Split(new string[] { " & ", "," },StringSplitOptions.None);
+            List<Wrestler> participants = new List<Wrestler>();
+            foreach (string wrestler in wrestlers)
+            {
+                if(wrestler.Length == 0) { continue; }
+                Wrestler w = new Wrestler();
+                w.name = Between(wrestler, ">", "<");
+                int.TryParse(Between(wrestler, "nr=", "&"),out w.wrestlerID);
+                participants.Add(w);
+            }
+            return participants;
+        }
     }
+
+    
 }
