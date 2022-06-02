fa5abb28feca8229da89315ccbfc2f38811b877e
ChrisAshtear <chrisashtear@gmail.com>
2022-06-01 23:01:28 -0400

    scraped some data from cagematch

--- CageMatchScraper/Program.cs
+++ CageMatchScraper/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CageMatchScraper;
+
+Scraper s = new Scraper();
+
+//List<string> list = s.ParseHtml(response);
+//var response2 = Scraper.GetWrestler(16006).Result;
+var response2 = Scraper.GetEntry(RequestType.PROMOTION,2287);
+Dictionary<string, string> data = s.ParseEntry (response2, "InformationBoxTitle", "InformationBoxContents");
+Console.WriteLine(response2);
---
+++ CageMatchScraper/Scraper.cs
@@ -0,0 +1,68 @@
+﻿using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.IO;
+
+
+
+namespace CageMatchScraper
+{
+    public enum RequestType
+    {
+        EVENT = 1,
+        WRESTLER = 2,
+        PROMOTION = 8,
+        TITLE = 5
+    }
+
+    public class Scraper
+    {
+        private static string baseURL = "https://www.cagematch.net/?";// id=2&nr=16006";
+        private string url;
+        public Scraper()
+        {
+        }
+
+        public static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Host = new Uri(fullUrl).Host;
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+
+        public static string GetEntry(RequestType type, int promotionID)
+        {
+            string url = baseURL + "id="+(int)type+"&nr=" + promotionID;
+            return CallUrl(url).Result;
+        }
+
+        public Dictionary<string, string> ParseEntry(string html, string className, string classValue)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            var names = htmlDoc.DocumentNode.Descendants("div")
+                    .Where(node => node.GetAttributeValue("class", "").Contains(className)).ToList();
+
+            var values = htmlDoc.DocumentNode.Descendants("div")
+                    .Where(node => node.GetAttributeValue("class", "").Contains(classValue)).ToList();
+
+
+            Dictionary<string,string> data  = new Dictionary<string, string>();
+
+            for(int i = 0; i < names.Count; i++)
+            {
+                var name = names[i];
+                var value = values[i];
+                data.Add(name.InnerHtml.Trim(':'), value.InnerHtml);
+            }
+
+            return data;
+
+        }
+
+    }
+}
